0136beb813d130491dfc044962267c6ae6b24967
Kyle Feldman <flip93wolf@gmail.com>
2022-11-04 14:44:13 -0700

    Got search to update with another button upon hitting check weather conditions button, trying to get icons displayed now

--- assets/script.js
+++ assets/script.js
@@ -1,212 +1,242 @@
 /*Needs to reach MVP
-1. need date to be in 5 day format, not 3 hour 
-2. DONE: need seached cities to have clickable button showing localStorage data
+1. DONE: (needed secondary API) need date to be in 5 day format, not 3 hour 
+2. DONE: need seached cities to have clickable button using localStorage data
 3. need icons to get displayed
-4. need to show all searched cities ( maybe up to ten? and may need for-loop?)
-5. 
-6.
-7.
-8.
-9.
-10.
+4. Cannot complete : need to show all searched cities ( maybe up to ten? and may need for-loop?) 
+  4.a. Got user search to update DOM as soon as serach button is clicked, but i need a list of the last few cities searched
 */
-// API
-
-//need event button listener on search click ( or form submit?)
-
 //Hooks
 var todayWeatherEl = document.getElementById("todayWeather");
 var forecastEl = document.querySelector("#forecastBox");
 var goBtn = document.querySelector(".searchBtn");
 var queryHistoryEl = document.querySelector(".searchHistory");
+var queryHistoryExtra = document.querySelector(".searchHistoryExtra");
 var selectHistoryEl = document.querySelector(".openLastQuery");
-var today = moment()
-// $("#id or .class").text(today.format("MMM Do, YYYY"));
+
 goBtn.addEventListener("click", function getApi() {
   var userSearch = document.querySelector(".queryInput").value;
+  function storeClick() {
+    storeQuery();
+    function storeQuery() {
+      var storedCity = {
+        userSearch: document.querySelector(".queryInput").value.trim(),
+      };
+      localStorage.setItem("storedCity", JSON.stringify(storedCity));
+      console.log(storedCity);
+    }
+  }
+  storeClick();
+  showLastQuery();
   var requestUrl =
-    `https:api.openweathermap.org/data/2.5/forecast?units=imperial&appid=9ecdb8aa9746c79a02aea67b96d3c5a2&cnt=32&q=` +
+    "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=9ecdb8aa9746c79a02aea67b96d3c5a2&q=" +
     userSearch;
-  //main code to incorperate ^^^ // Main API code line works!!!!!
+  //main code to incorperate ^^^ // 5 Day API code line works!!!!!
   //Try to add an createElement for user input to display as History button?
+
   fetch(requestUrl)
     .then(function (response) {
       // console.log(response) // we are getting a response
       return response.json();
     })
     .then(function (data) {
-      console.log(data); // we are getting Data
-      // console.log(data.list[0].main.humidity + "%"); //sample pathway for aquiring specific data (humidity in this case)
-      // console.log(data.list[0].wind.speed + " Mph");
-      // console.log(data.list[0].main.temp + " Degrees");
+      console.log(data);
       var todayForecast = document.createElement("ul");
       todayForecast.classList.add("test");
       todayForecast.setAttribute("style", "color:white;");
       todayWeatherEl.appendChild(todayForecast);
-      //city name
-      var todayName = document.createElement("h5");
-      todayName.textContent = data.city.name;
-      todayForecast.appendChild(todayName);
-      //date
-      var todayDate = document.createElement("h5");
-      todayDate.textContent = data.list[0].dt_txt;
-      todayForecast.appendChild(todayDate);
-      //icon
-      var todayIcon = document.createElement("li");
-      todayIcon.textContent =
-        data.list[0].weather.icon + " :Icon should be here";
+      //city name ********************************************************
+      var cityName = document.createElement("h5");
+      cityName.textContent = data.name;
+      todayForecast.appendChild(cityName);
+      //date ********************************************************
+      var todaySlot = document.createElement("h5");
+      todaySlot.innerHTML = moment.unix(data.dt).format("MMM Do, YYYY"); // leave this alone // had to add.innerHTML to make this a "node"(?)
+      // console.log(
+      //   todaySlot.innerHTML +
+      //     " // I am showing the conversion of the Unix TimeStamp"
+      // ); // converting unix timestamp into readable date getting correct date to show in console.
+      todayForecast.appendChild(todaySlot);
+      //icon ********************************************************
+      var todayIcon = document.createElement("i"); // changed from "li" to "img" // changed from img to i
+      todayIcon.innerHTML = 'http://openweathermap.org/img/wn/'+data.weather[0].icon+'@2x.png';
+      console.log(data.weather[0].icon)
+      // "http://openweathermap.org/img/wn/10d@2x.png" //example URL path from documentation
       todayForecast.appendChild(todayIcon);
-      //humidity
+      console.log(todayIcon)
+      //humidity ********************************************************
       var todayHumidity = document.createElement("li");
-      todayHumidity.textContent =
-        "Humidity: " + data.list[0].main.humidity + "%";
+      todayHumidity.textContent = "Humidity: " + data.main.humidity + "%";
       todayForecast.appendChild(todayHumidity);
-      //temp
+      //temp ********************************************************
       var todayTemp = document.createElement("li");
-      todayTemp.textContent =
-        "Temperature: " + data.list[0].main.temp + " Degrees";
+      todayTemp.textContent = "Temperature: " + data.main.temp;
       todayForecast.appendChild(todayTemp);
-      //wind speed
+      //wind speed ********************************************************
       var todayWind = document.createElement("li");
-      todayWind.textContent = "Wind Speed: " + data.list[0].wind.speed + " Mph";
+      todayWind.textContent = "Wind: " + data.wind.speed + " Mph";
       todayForecast.appendChild(todayWind);
+    });
+  ///********* 5 day Forecast *********\\\
 
-      // for (var i = 1; i < data.list.length; i++) -- original for loop set up  
-      for (var i = 1; i < data.list.length; i+8)  {
+  var Url5day =
+    "https:api.openweathermap.org/data/2.5/forecast?units=imperial&appid=9ecdb8aa9746c79a02aea67b96d3c5a2&cnt=40&q=" +
+    userSearch;
+  fetch(Url5day)
+    .then(function (response) {
+      // console.log(response) // we are getting a response
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      // console.log(data.list[0].main.humidity + "%");
+      // console.log(data.list[0].wind.speed + " Mph");
+      // console.log(data.list[0].main.temp);
+      for (var i = 0; i < data.list.length; i += 8) {
+        // console.log(i);
+        // pre-setup ********************************************************
         var showData = document.createElement("ul");
         showData.classList.add("test");
         showData.setAttribute("style", "color:white;");
         forecastEl.appendChild(showData);
-        //date
+        //Date********************************************************
         var todayDate = document.createElement("h6");
-        todayDate.textContent = data.list[i].dt;
+        todayDate.textContent = data.list[i].dt_txt;
         showData.appendChild(todayDate);
-        console.log(todayDate)
-        //icon
-        var todayIcon = document.createElement("li");
-        todayIcon.textContent =
+        //icon********************************************************
+        var fiveDayIcon = document.createElement("i");
+        // fiveDayIcon.innerHTML ='http://openweathermap.org/img/wn/'+data.list[i].weather[i].icon+'@2x.png';
+        fiveDayIcon.innerHTML =
           data.list[i].weather.icon + " Icon should be here";
-        showData.appendChild(todayIcon);
-        // console.log(todayIcon.img); // console.logging nothing/ undefined
-        //humidity
+        showData.appendChild(fiveDayIcon);
+        // console.log(fiveDayIcon.img); // console.logging nothing/ undefined
+        //humidity********************************************************
         var humidityForecast = document.createElement("li");
         humidityForecast.textContent =
           "Humidity: " + data.list[i].main.humidity + "%";
         showData.appendChild(humidityForecast);
-        //temp
+        //temp********************************************************
         var tempForecast = document.createElement("li");
-        tempForecast.textContent =
-          "Temp: " + data.list[i].main.temp + " Degrees";
+        tempForecast.textContent = "Temp: " + data.list[i].main.temp;
         showData.appendChild(tempForecast);
-        //wind speed
+        //wind speed********************************************************
         var windForecast = document.createElement("li");
-        windForecast.textContent =
-          "Wind Speed: " + data.list[i].wind.speed + " MPH";
+        windForecast.textContent = "Wind: " + data.list[i].wind.speed + " MPH";
         showData.appendChild(windForecast);
       }
     });
 });
 
 selectHistoryEl.addEventListener("click", function getApi() {
-  // var userSearch = document.querySelector(".queryInput").value;
   let pulledCity = JSON.parse(localStorage.getItem("storedCity")); // this should be storing the value (e.G. pulledCity = CityName)
-  console.log(pulledCity) // this is logging {userSearch: 'Juno'}
+  console.log(pulledCity); // this is logging {userSearch: 'Juno'}
   var requestUrl =
-    `https:api.openweathermap.org/data/2.5/forecast?units=imperial&appid=9ecdb8aa9746c79a02aea67b96d3c5a2&cnt=6&q=`+
+    "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=9ecdb8aa9746c79a02aea67b96d3c5a2&q=" +
     pulledCity.userSearch; //added userSearch to the end here and it will function *correctly*
-    console.log(requestUrl)
+  console.log(requestUrl);
   fetch(requestUrl)
     .then(function (response) {
+      // console.log(response) // we are getting a response
       return response.json();
     })
     .then(function (data) {
-      var todayForecast = document.createElement("ul");
+      console.log(data);
+      let todayForecast = document.createElement("ul");
       todayForecast.classList.add("test");
       todayForecast.setAttribute("style", "color:white;");
       todayWeatherEl.appendChild(todayForecast);
-      //city name
-      var todayName = document.createElement("h5");
-      todayName.textContent = data.city.name;
-      todayForecast.appendChild(todayName);
-      //date 1. need date to be in 5 day format, not 3 hour 
-      var todayDate = document.createElement("h5");
-      todayDate.textContent = data.list[0].dt_txt;
-      todayForecast.appendChild(todayDate);
-      //icon 3. need icons to get displayed
-      var todayIcon = document.createElement("i");
-      todayIcon.textContent =
-        data.list[0].weather.icon + " :Icon should be here";
+      //city name ********************************************************
+      let cityName = document.createElement("h5");
+      cityName.textContent = data.name;
+      todayForecast.appendChild(cityName);
+      //date ********************************************************
+      let todaySlot = document.createElement("h5");
+      todaySlot.innerHTML = moment.unix(data.dt).format("MMM Do, YYYY"); // leave this alone // had to add.innerHTML to make this a "node"(?)
+      console.log(
+        todaySlot.innerHTML +
+          " // I am showing the conversion of the Unix TimeStamp"
+      ); // converting unix timestamp into readable date getting correct date to show in console.
+      todayForecast.appendChild(todaySlot);
+      //icon ********************************************************
+      let todayIcon = document.createElement("li");
+      todayIcon.textContent = data.weather.icon + " :Icon should be here";
       todayForecast.appendChild(todayIcon);
-      //humidity
-      var todayHumidity = document.createElement("li");
-      todayHumidity.textContent =
-        "Humidity: " + data.list[0].main.humidity + "%";
+      console.log(todayIcon.textContent);
+      //humidity ********************************************************
+      let todayHumidity = document.createElement("li");
+      todayHumidity.textContent = "Humidity: " + data.main.humidity + "%";
       todayForecast.appendChild(todayHumidity);
-      //temp
-      var todayTemp = document.createElement("li");
-      todayTemp.textContent =
-        "Temperature: " + data.list[0].main.temp + " Degrees";
+      //temp ********************************************************
+      let todayTemp = document.createElement("li");
+      todayTemp.textContent = "Temperature: " + data.main.temp;
       todayForecast.appendChild(todayTemp);
-      //wind speed
-      var todayWind = document.createElement("li");
-      todayWind.textContent = "Wind Speed: " + data.list[0].wind.speed + " Mph";
+      //wind speed ********************************************************
+      let todayWind = document.createElement("li");
+      todayWind.textContent = "Wind: " + data.wind.speed + " Mph";
       todayForecast.appendChild(todayWind);
+    });
 
-      for (var i = 1; i < data.list.length; i++) {
+  ///********* 5 day Forecast *********\\\
+
+  var Url5day =
+    "https:api.openweathermap.org/data/2.5/forecast?units=imperial&appid=9ecdb8aa9746c79a02aea67b96d3c5a2&cnt=40&q=" +
+    pulledCity.userSearch;
+  fetch(Url5day)
+    .then(function (response) {
+      // console.log(response) // we are getting a response
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      // console.log(data.list[0].main.humidity + "%");
+      // console.log(data.list[0].wind.speed + " Mph");
+      // console.log(data.list[0].main.temp);
+      for (var i = 0; i < data.list.length; i += 8) {
+        // console.log(i);
+        // pre-setup ********************************************************
         var showData = document.createElement("ul");
         showData.classList.add("test");
         showData.setAttribute("style", "color:white;");
         forecastEl.appendChild(showData);
-        //date
+        //Date********************************************************
         var todayDate = document.createElement("h6");
         todayDate.textContent = data.list[i].dt_txt;
         showData.appendChild(todayDate);
-        //icon 3. need icons to get displayed
-        var todayIcon = document.createElement("li");
-        todayIcon.textContent =
+        //icon********************************************************
+        var fiveDayIcon = document.createElement("li");
+        fiveDayIcon.innerHTML =
           data.list[i].weather.icon + " Icon should be here";
-        showData.appendChild(todayIcon);
-        console.log(todayIcon.img); // console.logging nothing/ undefined
-        //humidity
+        showData.appendChild(fiveDayIcon);
+        // console.log(fiveDayIcon.img); // console.logging nothing/ undefined
+        //humidity********************************************************
         var humidityForecast = document.createElement("li");
         humidityForecast.textContent =
           "Humidity: " + data.list[i].main.humidity + "%";
         showData.appendChild(humidityForecast);
-        //temp
+        //temp********************************************************
         var tempForecast = document.createElement("li");
-        tempForecast.textContent =
-          "Temp: " + data.list[i].main.temp + " Degrees";
+        tempForecast.textContent = "Temp: " + data.list[i].main.temp;
         showData.appendChild(tempForecast);
-        //wind speed
+        //wind speed********************************************************
         var windForecast = document.createElement("li");
-        windForecast.textContent =
-          "Wind Speed: " + data.list[i].wind.speed + " MPH";
+        windForecast.textContent = "Wind: " + data.list[i].wind.speed + " MPH";
         showData.appendChild(windForecast);
       }
-      // icon.replace();
     });
 });
 
-goBtn.addEventListener("click", function storeClick(event) {
-  event.preventDefault();
-  storeQuery();
-  function storeQuery() {
-    // for (i = 0; i < 8; i++) {}
-      var storedCity = {
-        userSearch: document.querySelector(".queryInput").value.trim(),
-      };
-      localStorage.setItem("storedCity", JSON.stringify(storedCity));
-      console.log(storedCity);
-  }
-});
-
-// 2. need seached cities to have clickable button showing localStorage data
-// 4. need to show all searched cities ( maybe up to ten? and may need for-loop?)
 function showLastQuery() {
   var pulledCity = JSON.parse(localStorage.getItem("storedCity"));
+  // console.log(pulledCity)
   if (pulledCity !== null) {
     document.querySelector(".searchHistory").innerHTML = pulledCity.userSearch;
   }
+  for (var i = 0; i < pulledCity.userSearch.length; i++) {
+    pulledCity.userSearch = document.createElement("button");
+    pulledCity.textContent = document.querySelector(".searchHistory").value;
+    pulledCity.textContent = JSON.parse(localStorage.getItem("storedCity"))
+    queryHistoryExtra.appendChild(pulledCity.userSearch);
+  }
+  // console.log(pulledCity.textContent)
 }
 function init() {
   showLastQuery();
@@ -232,4 +262,30 @@ init();
       console.log(data)
     })
     getApi()
+});*/
+
+//NOTE : this wont work ( displays the current day at start of flex box instead of next day, no idea why, i
+// have changed the variable names, i have the correct pathways for the list,
+// but it keeps showing todays date in the first box of the five day forecast)
+// var fiveDaySlot = document.createElement("h5");
+// fiveDaySlot.innerHTML = moment
+//   .unix(data.list[i].dt_txt)
+//   .format("MMM Do, YYYY");
+// console.log(
+//   fiveDaySlot +
+//     " // This is one of the five day conversion of the Unix TimeStamp"
+// );
+// showData.appendChild(fiveDaySlot);
+
+/*goBtn.addEventListener("click", function storeClick(event) {
+  event.preventDefault();
+  storeQuery();
+  function storeQuery() {
+    // for (i = 0; i < 8; i++) {}
+    var storedCity = {
+      userSearch: document.querySelector(".queryInput").value.trim(),
+    };
+    localStorage.setItem("storedCity", JSON.stringify(storedCity));
+    console.log(storedCity);
+  }
 });*/
